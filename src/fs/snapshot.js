4df68f85ca711ac8652621bd0533b971a0d6e86e
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-03-11 00:06:54 +0200

    feat: implement fs:snapshot

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,9 +1,46 @@
+import { readdir, readFile, stat, writeFile } from 'node:fs/promises';
+import path, { join } from 'node:path';
+
+const rootPath = process.cwd();
+
+const WORKSPACE_PATH = join(rootPath, 'workspace');
+const FILE_PATH = join(rootPath, 'snapshot.json');
+
+const checkIsFolderExists = async (path) => {
+	try {
+		const stats = await stat(path);
+		return stats.isDirectory();
+	} catch {
+		return false;
+	}
+};
+
 const snapshot = async () => {
-  // Write your code here
-  // Recursively scan workspace directory
-  // Write snapshot.json with:
-  // - rootPath: absolute path to workspace
-  // - entries: flat array of relative paths and metadata
+	const isWorkspacePathExists = await checkIsFolderExists(WORKSPACE_PATH);
+	if (!isWorkspacePathExists) throw new Error('FS operation failed');
+
+	const entries = await Promise.all(
+		(await readdir(WORKSPACE_PATH, { recursive: true })).map(async (file) => {
+			const dirData = await stat(join(WORKSPACE_PATH, file));
+			return dirData.isFile()
+				? {
+						path: path.normalize(file).replaceAll(path.sep, '/'),
+						type: 'file',
+						size: dirData.size,
+						content: await readFile(join(WORKSPACE_PATH, file), { encoding: 'base64' }),
+					}
+				: {
+						path: path.normalize(file).replaceAll(path.sep, '/'),
+						type: 'directory',
+					};
+		}),
+	);
+	const snapshot = JSON.stringify(
+		{ rootPath: path.normalize(WORKSPACE_PATH).replaceAll(path.sep, '/'), entries },
+		null,
+		3,
+	);
+	await writeFile(FILE_PATH, snapshot);
 };
 
 await snapshot();
