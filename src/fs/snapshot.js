ca93f83be395ac907f9d0587199dbb849eb30be6
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-03-11 10:50:23 +0200

    refactor: update fs:snapshot implementation

--- src/fs/snapshot.js
+++ src/fs/snapshot.js
@@ -1,10 +1,10 @@
 import { readdir, readFile, stat, writeFile } from 'node:fs/promises';
-import path, { join } from 'node:path';
+import path from 'node:path';
 
 const rootPath = process.cwd();
 
-const WORKSPACE_PATH = join(rootPath, 'workspace');
-const FILE_PATH = join(rootPath, 'snapshot.json');
+const WORKSPACE_PATH = path.join(rootPath, 'workspace');
+const FILE_PATH = path.join(rootPath, 'snapshot.json');
 
 const checkIsFolderExists = async (path) => {
 	try {
@@ -19,28 +19,33 @@ const snapshot = async () => {
 	const isWorkspacePathExists = await checkIsFolderExists(WORKSPACE_PATH);
 	if (!isWorkspacePathExists) throw new Error('FS operation failed');
 
-	const entries = await Promise.all(
-		(await readdir(WORKSPACE_PATH, { recursive: true })).map(async (file) => {
-			const dirData = await stat(join(WORKSPACE_PATH, file));
-			return dirData.isFile()
-				? {
-						path: path.normalize(file).replaceAll(path.sep, '/'),
-						type: 'file',
-						size: dirData.size,
-						content: await readFile(join(WORKSPACE_PATH, file), { encoding: 'base64' }),
-					}
-				: {
-						path: path.normalize(file).replaceAll(path.sep, '/'),
-						type: 'directory',
-					};
-		}),
-	);
-	const snapshot = JSON.stringify(
+	const workspaceDir = await readdir(WORKSPACE_PATH, { recursive: true });
+
+	const entries = [];
+	for (const dir of workspaceDir) {
+		const dirData = await stat(path.join(WORKSPACE_PATH, dir));
+		const dirPath = path.normalize(dir).replaceAll(path.sep, '/');
+		const entry = dirData.isFile()
+			? {
+					path: dirPath,
+					type: 'file',
+					size: dirData.size,
+					content: await readFile(path.join(WORKSPACE_PATH, dir), { encoding: 'base64' }),
+				}
+			: {
+					path: dirPath,
+					type: 'directory',
+				};
+		entries.push(entry);
+	}
+
+	const snapshotData = JSON.stringify(
 		{ rootPath: path.normalize(WORKSPACE_PATH).replaceAll(path.sep, '/'), entries },
 		null,
 		3,
 	);
-	await writeFile(FILE_PATH, snapshot);
+
+	await writeFile(FILE_PATH, snapshotData);
 };
 
 await snapshot();
