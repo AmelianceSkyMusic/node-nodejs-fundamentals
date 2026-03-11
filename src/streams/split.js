d6d74d64bb4d1f549095800568cb7ad3a9641dec
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-03-12 00:38:44 +0200

    feat: implement streams:split

--- src/streams/split.js
+++ src/streams/split.js
@@ -1,8 +1,57 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { argv } from 'node:process';
+import readline from 'node:readline/promises';
+
+const rootPath = process.cwd();
+
+const SOURCE_PATH = path.join(rootPath, 'source.txt');
+const ARG_PREFIX = '--';
+const ARG_PREFIX_REPLACEMENT = '';
+
+function parseArgs() {
+	let filteredArgs = {};
+
+	for (let i = 0; i < argv.length; i++) {
+		const argKey = argv[i];
+		if (argKey.startsWith(ARG_PREFIX)) {
+			const preparedArgKey = argKey.replace(ARG_PREFIX, ARG_PREFIX_REPLACEMENT);
+			const argValue = argv[i + 1];
+			filteredArgs[preparedArgKey] = argValue;
+			i++;
+		}
+	}
+
+	return filteredArgs;
+}
+
+const args = parseArgs();
+const chunkSize = Number(args.lines) || 10;
+
+let fileCount = 1;
+let chunksCount = 0;
+
 const split = async () => {
-  // Write your code here
-  // Read source.txt using Readable Stream
-  // Split into chunk_1.txt, chunk_2.txt, etc.
-  // Each chunk max N lines (--lines CLI argument, default: 10)
+	const input = createReadStream(SOURCE_PATH);
+
+	const rl = readline.createInterface({
+		input,
+		crlfDelay: Infinity,
+	});
+
+	let output;
+	for await (const line of rl) {
+		if (chunksCount % chunkSize === 0) {
+			if (output) output.end();
+			output = createWriteStream(path.join(rootPath, `chunk_${fileCount}.txt`));
+			fileCount++;
+		}
+		if (output) output.write(line + '\n');
+
+		chunksCount++;
+	}
+
+	if (output) output.end();
 };
 
 await split();
