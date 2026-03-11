a49e280311c50b6524d581f31983765378cc30f1
Ameliance SkyMusic <AmelianceSkyMusic@gmail.com>
2026-03-11 22:56:47 +0200

    feat: implement streams:lineNumberer

--- src/streams/lineNumberer.js
+++ src/streams/lineNumberer.js
@@ -1,8 +1,27 @@
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+let index = 0;
+
+const transformStream = new Transform({
+	transform(chunk, encoding, callback) {
+		const text = String(chunk);
+
+		const transformedText = Array.from(text.split('\n'))
+			.map((line) => {
+				index++;
+				return `${index} | ${line}`;
+			})
+			.join('\n');
+		this.push(transformedText);
+
+		callback();
+	},
+});
+
 const lineNumberer = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Use Transform Stream to prepend line numbers
-  // Write to process.stdout
+	pipeline(stdin, transformStream, stdout);
 };
 
 lineNumberer();
